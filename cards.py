025f0626678900d2a353955ee02494fe4308c6cb
Rogers <Michael.Rogers@mines.sdsmt.edu>
2020-04-11 15:06:08 -0600

    04/11/2020:15:06:08.56

--- cards.py
+++ cards.py
@@ -4,10 +4,9 @@ import random
 This class is just a list for holding cards
 """
 class Cards:
-    cards = []
     # __init__    ->  Class instantiation, no arguments
-    def __init__(self):
-        self.cards
+    def __init__(self, decks = 0):
+        self.cards = []
 
     # __str__     ->  The string representation of the cards object for printing
     def __str__(self):
@@ -15,7 +14,6 @@ class Cards:
 
     # add_deck    ->  One optional argument to specify adding more than one 52
     #                 card deck to the cards
-    @classmethod
     def add_deck(self, decks = 1):
         # First I define two lists, one with the numbers and one with suits
         ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
@@ -27,21 +25,18 @@ class Cards:
 
     # add_joker   ->  One optional argument to specify adding more than one
     #                 joker to the deck
-    @classmethod
     def add_joker(self, jokers = 1):
         # For the number of jokers specified, add a joker to the top of the pile
         for i in range(jokers):
             self.cards.append('J')
     
     # shuffle     ->  Randomly shuffle the cards
-    @classmethod
     def shuffle(self):
         random.shuffle(self.cards)
 
     # sort        ->  Arrange the cards in order by suit, one argument that
     # specifies whether you like the values right to left 'rtl' default, or left
     # to right 'ltr', otherwise known as reverse order for pagans
-    @classmethod
     def sort(self, direction='rtl'):
         # First we need to sort the cards by suit
         self.cards.sort(key=Cards.suit_key)
@@ -56,19 +51,15 @@ class Cards:
 
     # These properties all return the number of the suit in a hand
     # The property decorator means no parentheses needed when called
-    @classmethod
     def spades(self):
         return len(list(filter(lambda x: True if x[-1] is 'S' else False,
         self.cards)))
-    @classmethod
     def hearts(self):
         return len(list(filter(lambda x: True if x[-1] is 'H' else False,
         self.cards)))
-    @classmethod
     def clubs(self):
         return len(list(filter(lambda x: True if x[-1] is 'C' else False,
         self.cards)))
-    @classmethod
     def diamonds(self):
         return len(list(filter(lambda x: True if x[-1] is 'D' else False,
         self.cards)))
@@ -86,7 +77,6 @@ class Cards:
     #                 card to a new hand, then return a tuple of the amount of
     #                 hands that were specified. Deal the number of cards
     #                 specified by hand_size
-    @classmethod
     def deal(self, hands, hand_size):
         # Create an empty tuple of size hands
         dealt_hands = tuple(Cards() for i in range(hands))
@@ -101,8 +91,10 @@ deck = Cards()
 deck.add_deck()
 deck.add_joker()
 deck.shuffle()
+
 (hand,) = deck.deal(1, 10)
 
+print(deck)
 print(hand)
 hand.sort()
 print(hand)
